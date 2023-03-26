80425c155c77391167e41cd15b48532c1b155eef
Gabriel Santiago <gaaasts@gmail.com>
2023-03-26 19:21:15 -0300

    test bug fixed

--- Tests/viewcoding-packageTests/viewcoding_packageTests.swift
+++ Tests/viewcoding-packageTests/viewcoding_packageTests.swift
@@ -6,6 +6,5 @@ final class viewcoding_packageTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(viewcoding_package().text, "Hello, World!")
     }
 }
